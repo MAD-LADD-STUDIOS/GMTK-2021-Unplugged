69a1d74364e989a9b0a1ec4bf312d527e695adf6
Code Crash <50129568+CodeCrashStudios@users.noreply.github.com>
2021-06-11 18:14:51 -0400

    Added Collision Detection and smooth movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,71 +4,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] GameObject playerLeft;
-    [SerializeField] GameObject playerRight;
-    [SerializeField] Vector2Int posLeft;
-    [SerializeField] Vector2Int posRight;
-
-
-    [SerializeField] Vector2 clampX; // min, max
-    [SerializeField] Vector2 clampY; // min, max
-    // scale: 7x7 per grid (atm)
-    // middle: 4, 4
+    [SerializeField] float moveTime = 1.0f;
+    float currentTime = 0.0f;
+    Vector3 currentPos;
+    Vector3 newPos;
+    bool moving = false;
 
     void Start()
     {
-        posLeft = new Vector2Int(4, 4);
-        posRight = new Vector2Int(4, 4);
+
     }
+
+
     void Update()
     {
-        var horizontal = Input.GetKeyDown(KeyCode.A) ? -1 : Input.GetKeyDown(KeyCode.D) ? 1 : 0;
-        var vertical = Input.GetKeyDown(KeyCode.S) ? -1 : Input.GetKeyDown(KeyCode.W) ? 1 : 0;
+        if (moving)
+        {
+            this.transform.position = Vector3.LerpUnclamped(currentPos, newPos, currentTime/moveTime);
+            currentTime += Time.deltaTime;
 
-        if(Mathf.Abs(horizontal) < 0.5f)
-            horizontal = 0;
+            if (currentTime >= moveTime) moving = false;
+        }
         else
-            horizontal = horizontal / Mathf.Abs(horizontal); // math magic
-        
-        if(Mathf.Abs(vertical) < 0.5f)
-            vertical = 0;
-        else
-            vertical = vertical / Mathf.Abs(vertical); // math magic
-        
-        /* NOTE
-        refer to https://answers.unity.com/questions/383671/find-gameobject-at-position.html
-        for solid walls in the future
-        */
-
-        // left player check
-        if(!(posLeft.x + horizontal >= clampX.y || posLeft.x + horizontal < clampX.x))
         {
-            playerLeft.transform.position += Vector3.right * horizontal;
-            posLeft += Vector2Int.right * horizontal;
+            if (Input.GetKeyDown(KeyCode.W) && !Physics2D.OverlapCircle(this.transform.position + Vector3.up, .2f))
+            {
+                newPos = this.transform.position + Vector3.up;
+                move();
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.A) && !Physics2D.OverlapCircle(this.transform.position + Vector3.left, .2f))
+            {
+                newPos = this.transform.position + Vector3.left;
+                move();
+            }
+
+            if (Input.GetKeyDown(KeyCode.S) && !Physics2D.OverlapCircle(this.transform.position + Vector3.down, .2f))
+            {
+                newPos = this.transform.position + Vector3.down;
+                move();
+            }
+
+            if (Input.GetKeyDown(KeyCode.D) && !Physics2D.OverlapCircle(this.transform.position + Vector3.right, .2f))
+            {
+                newPos = this.transform.position + Vector3.right;
+                move();
+            }
         }
-
-        if(!(posLeft.y + vertical >= clampY.y || posLeft.y + vertical <= clampY.x))
-        {
-            playerLeft.transform.position += Vector3.up * vertical;
-            posLeft += Vector2Int.up * vertical;
-        }
-
-        // right player check
-        if(!(posRight.x + horizontal >= clampX.y || posRight.x + horizontal < clampX.x))
-        {
-            playerRight.transform.position += Vector3.right * horizontal;
-            posRight += Vector2Int.right * horizontal;
-        }
-
-        if(!(posRight.y + vertical >= clampY.y || posRight.y + vertical <= clampY.x))
-        {
-            playerRight.transform.position += Vector3.up * vertical;
-            posRight += Vector2Int.up * vertical;
-        }
-
-        // not very flexible grid size, TODO fix
-        playerLeft.transform.position = (posLeft - (8 * Vector2.right) - (4* Vector2.up));
-        playerRight.transform.position = (posRight - (4*Vector2.up));
-
     }
+
+    void move()
+    {
+        currentPos = this.transform.position;
+        currentTime = 0.0f;
+        moving = true;
+    }
+
 }
